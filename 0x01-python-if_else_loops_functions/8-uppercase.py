b66c1b6ea67a4646b04bd50646653d58b71be84e
IdiJr <idialijnr@gmail.com>
2023-05-11 10:36:01 +0000

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
     for i in range(len(c)):
         if ord(c[i]) > 96 and ord(c[i]) < 123:
             c[i] = chr(ord(c[i]) - 32)
-            
+
     print("{}".format(("").join(c)))
