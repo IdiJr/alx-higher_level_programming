1304ebc830812e17cc8243ac084afed076c578f2
IdiJr <idialijnr@gmail.com>
2023-05-11 18:43:17 +0000

    ByteCode -> Python #3

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     """does exactly the same as the code provided"""
-    from magic_calculation_102 import add, sub
+    from magic_calculation import add, sub
 
     if a < b:
         c = add(a, b)
