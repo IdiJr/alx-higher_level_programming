8c94b6d58aca49f048ca74f359b8b34af9287e21
IdiJr <idialijnr@gmail.com>
2023-06-06 22:04:59 +0000

    test files

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""Unittest for max_integer([...]) 
+"""
+
+import unittest
+
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """Class unittest for the max integer function"""
+
+    def test_sorted_list(self):
+        sorted_list = [1, 2, 3, 4]
+        self.assertEqual(max_integer(sorted_list), 4)
+
+    def test_unsorted_list(self):
+        unsorted_list = [1, 2, 3, 4]
+        self.assertEqual(max_integer(unsorted_list), 4)
+
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_max_first(self):
+        max_first = [4, 1, 2, 3]
+        self.assertEqual(max_integer(max_first), 4)
+
+    def test_strings(self):
+        self.assertEqual(max_integer("school"), "s")
+        self.assertEqual(max_integer(""), None)
+
+if __name__ == '__main__':
+        unittest.main()
