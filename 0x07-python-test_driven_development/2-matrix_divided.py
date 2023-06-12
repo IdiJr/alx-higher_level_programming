4359ff0678adc78daeba8179c9640abc8779d775
IdiJr <idialijnr@gmail.com>
2023-06-12 11:41:16 +0000

    Matrix multiplication

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Function that divides all elements of a matrix."""
 
 
 def matrix_divided(matrix, div):
@@ -11,8 +12,8 @@ def matrix_divided(matrix, div):
         not all((isinstance(element, int) or isinstance(element, float))
                 for element in [number for row in matrix for number in row])):
 
-        raise TypeError("matrix must be a matrix (list of lists) of "
-                        "integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of
+                integers/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
