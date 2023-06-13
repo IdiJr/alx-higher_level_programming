3f57725845f6e36f26ebf4b99d5d60f97423d92f
IdiJr <idialijnr@gmail.com>
2023-06-13 12:15:02 +0000

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
+"""Function that prints a sorted list"""
 
 class MyList(list):
-    """Function that prints a sorted list"""
+    """Represents a public instance of type list"""
     def print_sorted(self):
         """Prints a sorted list"""
         print(sorted(self))
