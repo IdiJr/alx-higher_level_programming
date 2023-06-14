e65d7a69e7df288a9a02c9530ceeb666f8f14062
IdiJr <idialijnr@gmail.com>
2023-06-14 09:27:54 +0000

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""function that adds all arguments to a Python list and then save them to a file"""
+"""
+function that adds all arguments to a Python list,
+and then save them to a file
+"""
 import sys
 
 
