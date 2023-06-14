5053ecad11001b89d31a52d067534c9ea2281b3b
IdiJr <idialijnr@gmail.com>
2023-06-14 05:56:16 +0000

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""function that writes a string to a text file"""
+
+def write_file(filename="", text=""):
+    """function that counts characters
+    Args:
+        Arg filename: name of file
+        Arg text: text to be counted
+    Return: writes into the specified file
+    """
+    with open(filename, "w", encoding="UTF-8") as FILE:
+        written_FILE = FILE.write(text)
+        return (written_FILE)
