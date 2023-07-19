4fdfa0196dbbf300aa379ac8b436a5c1655b9b41
IdiJr <idialijnr@gmail.com>
2023-07-19 18:15:42 +0000

    Genre ID for all shows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- lists all shows in db hbtn_0d_tvshows
+-- and should tv_shows.title and tv_show_genres.genre_id
+-- must be sorted by tv_shows.title and tv_show_genres.genre_id
+-- shows without genre displays NULL
+SELECT tv_shows.title, tv_show_genres.genre_id
+  FROM tv_shows AS tv_shows
+	 LEFT JOIN tv_show_genres AS tv_show_genres
+	    ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
