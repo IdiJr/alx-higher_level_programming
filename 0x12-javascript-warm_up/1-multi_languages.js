84a6e244b9d7a74920b623fb27aa386e425497af
IdiJr <idialijnr@gmail.com>
2023-08-14 18:07:34 +0000

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+let firstVar = 'C is fun';
+let secondVar = 'Python is cool';
+let thirdVar = 'JavaScript is amazing';
+console.log(firstVar);
+console.log(secondVar);
+console.log(thirdVar);
