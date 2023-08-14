ae5fc1a8ba08a22ae8b01c143a3b66fc0d364079
IdiJr <idialijnr@gmail.com>
2023-08-14 19:00:44 +0000

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@
 if (process.argv.length === 2) {
   console.log('No arguments');
 }
-if (process.argv.length === 3) {
+else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
