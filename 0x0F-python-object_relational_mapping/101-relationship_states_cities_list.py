a320c6061457bb1ba364061673a36f3dd2062750
IdiJr <idialijnr@gmail.com>
2023-08-20 18:07:36 +0000

    List relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -6,7 +6,7 @@ contained in the database hbtn_0e_101_usa
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
+from relationship_state import State
 from relationship_city import City
 
 if __name__ == "__main__":
