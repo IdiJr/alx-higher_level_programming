b6fda506d11b49b9d9fe249601cb685d9166b41c
IdiJr <idialijnr@gmail.com>
2023-08-20 15:17:52 +0000

    Cities in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,10 +3,8 @@
 Defines the City class and creates the Base instance using SQLAlchemy.
 """
 
-from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
-
-Base = declarative_base()
 
 
 class City(Base):
