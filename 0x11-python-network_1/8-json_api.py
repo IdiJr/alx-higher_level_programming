bcce435c3227993207171d6b2b13ccb2831d4b88
IdiJr <idialijnr@gmail.com>
2023-09-02 05:29:41 +0000

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A python script that takes in a letter and send a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        letter = ""
+    else:
+        letter = sys.argv[1]
+    payload = {"q": letter}
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if not response:
+            print("Noo result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
