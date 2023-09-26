994dd9b4c771e43420a8021d05f67a4c510b5005
IdiJr <idialijnr@gmail.com>
2023-09-26 18:44:35 +0000

    Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,29 +1,24 @@
 #!/usr/bin/node
 const request = require('request');
 const movieId = process.argv[2];
-const link = `https://swapi.co/api/films/${movieId}`;
+const link = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 request(link, (err, response, body) => {
   if (err) {
     console.error(err);
     return;
   }
-  if (response.statusCode === 200) {
-    console.log('Characters in', JSON.parse(body).title);
-    const movieCharacters = JSON.parse(body).characters;
-    movieCharacters.forEach((movieCharacters) => {
-      request(movieCharacters, (charError, charResponse, charBody) => {
-        if (charError) {
-          console.error(charError);
-          return;
-        }
-        if (charResponse.statusCode === 200) {
-          console.log(JSON.parse(charBody).name);
-        } else {
-          console.error(`Failed to fetch character data. Status code: ${charResponse.statusCode}`);
-        }
-      });
+  const movieCharacters = JSON.parse(body).characters;
+  movieCharacters.forEach((movieCharacters) => {
+    request(movieCharacters, (charError, charResponse, charBody) => {
+      if (charError) {
+        console.error(charError);
+        return;
+      }
+      if (charResponse.statusCode === 200) {
+        console.log(JSON.parse(charBody).name);
+      } else {
+        console.error(`Failed to fetch character data. Status code: ${charResponse.statusCode}`);
+      }
     });
-  } else {
-    console.error(`Failed to fetch movie data. Status code: ${response.statusCode}`);
-  }
+  });
 });
